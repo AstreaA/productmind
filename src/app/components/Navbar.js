bbc1a03a2aaa358c94b1848a606d927c1a487f1d
astrea03 <danasonova@icloud.com>
2025-04-02 16:12:29 +0300

    add link in main page

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center p-6">
       
-        <h1 className="text-4xl font-bold text-[#f13c20]">ProductMind</h1>
+        <Link href={`../`} className="text-4xl font-bold text-[#f13c20]">ProductMind</Link>
         <div className="flex gap-4 text-[#d62828] font-bold">
           <a href="#" className="hover:underline mr-2 mt-3">Menu</a>
           <Image src="/images/menu.png" alt="Menu" width={50} height={50} />
--- src/app/courses/[id]/page.js
+++ src/app/courses/[id]/page.js
@@ -1,8 +1,8 @@
 import CourseContent from './CourseContent';
 
-// Define which course IDs should be pre-rendered at build time
+
 export async function generateStaticParams() {
-  // This would typically come from an API or database
+  
   const courseIds = [1, 2, 3];
   return courseIds.map((id) => ({
     id: id.toString(),
